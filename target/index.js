be3f420966cd1dcefa3fc148f1bf659b3dcc7d61
hakan0814 <h.k.cuhadar@gmail.com>
2019-11-12 11:32:57 +0100

    Routing HTTP requests implemented

--- target/index.js
+++ target/index.js
@@ -1,11 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const routing_controllers_1 = require("routing-controllers");
-const controller_1 = __importDefault(require("./controller"));
+const controller_1 = require("./pages/controller");
 const port = process.env.PORT || 4000;
 const app = routing_controllers_1.createKoaServer({
     controllers: [controller_1.default]
